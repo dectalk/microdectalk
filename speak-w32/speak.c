dde275a2ed3fcdea3ec5e78414cb7141816bee9b
NishiOwO <nishi@nishi.boats>
2025-09-16 07:41:42 +0900

    we should never commit wav

--- speak-w32/speak.c
+++ speak-w32/speak.c
@@ -1,6 +1,16 @@
 #include <windows.h>
 
+#include <icons/pau203a.xpm>
+#include <icons/bet203a.xpm>
+#include <icons/har203a.xpm>
+#include <icons/fra203a.xpm>
+#include <icons/den203a.xpm>
+#include <icons/kid203a.xpm>
+#include <icons/urs203a.xpm>
+#include <icons/rit203a.xpm>
+#include <icons/wen203a.xpm>
+
 #include <epsonapi.h>
 
-int main(){
+int WINAPI WinMain(HINSTANCE hCurInst, HINSTANCE hPrevInst, LPSTR lpsCmdLine, int nCmdShow){
 }
