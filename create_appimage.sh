fa023e5cd1e65b271c127a979e2cef6edf1adeb8
NishiOwO <nishi@nishi.boats>
2025-09-18 07:16:02 +0900

    might work

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
-if [ ! -f linuxdeploy ]; then
-	wget -O linuxdeploy https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20250213-2/linuxdeploy-x86_64.AppImage
+if [ ! -f appimagetool ]; then
+	wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 fi
-chmod +x linuxdeploy
+chmod +x appimagetool
 
 rm -rf appdir
-./linuxdeploy --appdir appdir --executable bin/Native/Release/speak --desktop-file resources/speak.desktop --icon-file resources/paul.png --output appimage
+mkdir -p appdir/usr/bin
+mkdir -p appdir/usr/lib/x86_64-linux-gnu
+
+ln -s usr/lib appdir/lib
+ln -s usr/lib appdir/lib64
+ln -s usr/lib appdir/usr/lib64
+
+cp bin/Native/Release/speak appdir/usr/bin/
+cp resources/speak.desktop appdir/
+cp resources/paul.png appdir/
+cp resources/AppRun appdir/
+cp DECtalk.conf appdir/
+cp -rf dic appdir/
+chmod +x appdir/AppRun
+
+ldd appdir/usr/bin/speak | while read a; do
+	path=`echo "$a" | grep -oE '/[^ ]+'`
+	if [ ! "x$path" = "x" ]; then
+		cp $path appdir/$path
+	fi
+done
+
+strip appdir/usr/lib/* appdir/usr/bin/*
+
+ARCH=x86_64
+export ARCH
+./appimagetool appdir Speak-x86_64.AppImage
