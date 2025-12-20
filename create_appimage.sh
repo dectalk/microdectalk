1f9a50d6a26f9d3b17d54afbbd9ef882dc4a280e
NishiOwO <nishi@nishi.boats>
2025-12-21 00:15:15 +0900

    maybe this one will work

--- create_appimage.sh
+++ create_appimage.sh
@@ -17,7 +17,12 @@ cp DECtalk.conf appdir/
 cp -rf dic appdir/
 chmod +x appdir/AppRun
 
-ldd appdir/usr/bin/speak | -E grep "=>|ld-" | awk '{print $3}' | while read a; do
+ldd appdir/usr/bin/speak | grep "=>" | awk '{print $3}' | while read a; do
+	if [ -f "$a" ]; then
+		cp "$a" appdir/usr/lib/
+	fi
+done
+ldd appdir/usr/bin/speak | grep "ld-" | awk '{print $1}' | while read a; do
 	if [ -f "$a" ]; then
 		cp "$a" appdir/usr/lib/
 	fi
