65afdd42ef395b260e8ea1152c0a46a9f5f027b5
Leondro Lio <github@leondrolio.com>
2026-03-26 00:00:01 +0000

    publish dectalk to nodejs (only for macos + linux)

--- platforms/node/src/test.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "node:fs"
-import say from ".";
-
-const buffer = say("This is a message from Node J S");
-fs.writeFileSync("first.wav", buffer);
-
-const buffer2 = say("This is a second message from Node J S");
-fs.writeFileSync("second.wav", buffer2);
-console.log("end of programme");
