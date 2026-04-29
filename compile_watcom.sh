abc6feb4c4bf66d7dd17f81f88eb20ab19ecabaf
NishiOwO <nishi@nishi.boats>
2026-04-29 21:53:27 +0900

    fix watcom

--- compile_watcom.sh
+++ compile_watcom.sh
@@ -26,7 +26,7 @@ for i in src/*.c $FILES; do
         if [ -f "$OUT" ]; then
                 continue
         fi
-        owcc -Wc-fx -c -b$TARGET $ARGS -Iinclude -Isrc -D_REENTRANT -DNOMME -DLTSSIM -DTTSSIM -DANSI  -DENGLISH -DENGLISH_US -DACCESS32 -DTYPING_MODE -DACNA -DDISABLE_AUDIO -DSINGLE_THREADED -o $OUT $i
+        owcc -Wc,-fx -c -b$TARGET $ARGS -Iinclude -Isrc -D_REENTRANT -DNOMME -DLTSSIM -DTTSSIM -DANSI  -DENGLISH -DENGLISH_US -DACCESS32 -DTYPING_MODE -DACNA -DDISABLE_AUDIO -DSINGLE_THREADED -o $OUT $i
 done
 
 if [ "$TARGET" = "nt_dll" ]; then
